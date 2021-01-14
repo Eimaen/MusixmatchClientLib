e8f862fa96f94846b88d3be9c261e6cf66e10f06
Eimaen <32714790+Eimaen@users.noreply.github.com>
2021-01-15 02:20:03 +0300

    .gitignore change, private info is not exposed anymore

---
+++ MCLTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using MusixmatchClientLib;
+using MusixmatchClientLib.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCLTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MusixmatchClient client = new MusixmatchClient(new MusixmatchToken());
+            var tracks = client.SongSearch("Tristam - Once Again"); // JSaB <3
+            var firstTrack = tracks.First();
+            var lyrics = client.GetSyncedLyrics(firstTrack.TrackId);
+            Console.WriteLine(lyrics.SubtitleBody);
+            Console.ReadKey();
+        }
+    }
+}
