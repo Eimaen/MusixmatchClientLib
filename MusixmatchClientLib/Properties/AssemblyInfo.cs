0e3b0653838648b512ab510e48794509c2c0ac9b
Eimaen <32714790+Eimaen@users.noreply.github.com>
2022-08-15 23:34:57 +0300

    bump version

--- MusixmatchClientLib/Properties/AssemblyInfo.cs
+++ MusixmatchClientLib/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("8723fab2-444e-436c-aee7-08f929e7dbaf")]
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyVersion("1.1.2.0")]
+[assembly: AssemblyFileVersion("1.1.2.0")]
